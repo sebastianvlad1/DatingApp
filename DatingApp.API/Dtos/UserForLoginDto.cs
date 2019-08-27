c22ca51f774e80f0f1bcb03a2d3886058ba02668
Travis CI <travis@travis-ci.org>
2019-08-27 20:10:40 +0300

    Added Loggin and JWT Token

---
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -0,0 +1,9 @@
+namespace DatingApp.API.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Username { set; get; }
+
+        public string Password { get; set; }
+    }
+}
--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You're password can't be shorter than 4 characters!")]
         public string Password { get; set; }
     }
 }
