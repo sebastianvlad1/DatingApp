d6289854cc55d1cac8cbdde013a9715586f1dd36
Travis CI <travis@travis-ci.org>
2019-09-06 13:58:03 +0300

    Added Home & Register component

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -73,7 +73,7 @@ namespace DatingApp.API.Controllers
             var tokenHandler = new JwtSecurityTokenHandler();
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
-
+            
             return Ok(new {
                 token = tokenHandler.WriteToken(token)
             });   
