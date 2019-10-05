8dcbad5ffaceacf1ad1ab1f15533c73205f7e3af
Travis CI <travis@travis-ci.org>
2019-10-05 18:57:36 +0300

    Added Angular Routing and Guards

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -8,7 +8,6 @@ import { JwtHelperService } from '@auth0/angular-jwt';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = "Dating Application";
   jwtHelper = new JwtHelperService();
   
   constructor(private authService: AuthService){}
