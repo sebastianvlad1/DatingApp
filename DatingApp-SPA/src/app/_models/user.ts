e3fa01deaca46dd489a97673627c98662bbcfa60
Travis CI <travis@travis-ci.org>
2019-10-14 21:49:03 +0300

    Showing user cards and creating user model in ng using Typescript

---
+++ DatingApp-SPA/src/app/_models/user.ts
@@ -0,0 +1,18 @@
+import { NumberValueAccessor } from "@angular/forms/src/directives";
+import { Photo } from "./photo";
+
+export interface User {
+    id: number;
+    username: string;
+    knownAs: string;
+    age: number;
+    gender: string;
+    created: Date;
+    photoUrl: string;
+    city: string;
+    country: string;
+    interests?: string;
+    introduction?: string;
+    lookingFor?: string;
+    photos?: Photo[];
+}
